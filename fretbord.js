9cdd51b3c3ecd8850e5f261d557a010520cf4558
iztok.sebic <iztok.sebic@niris.si>
2025-06-14 23:07:30 +0200

    Code cleanup, still more WIP.

--- fretbord.js
+++ fretbord.js
@@ -1,36 +1,69 @@
 class Note {
-	constructor(note, order, hasHalfstep) {
+	constructor(note, order) {
 		this.Note = note;
 		this.Order = order;
-		this.HasHalfstep = hasHalfstep;}
+	}
 
 	static makeNotesSharp() {
 		return [
-			new Note("A", 1, true), new Note("A#", 2, true), new Note("B", 3, false),
-			new Note("C", 4, true), new Note("C#", 5, true), new Note("D", 6, true),
-			new Note("D#", 7, true), new Note("E", 8, false), new Note("F", 9, true),
-			new Note("F#", 10, true), new Note("G", 11, true), new Note("G#", 12, true)
+			new Note("A", 1),	new Note("A#", 2),	new Note("B", 3),
+			new Note("C", 4),	new Note("C#", 5),	new Note("D", 6),
+			new Note("D#", 7),	new Note("E", 8),	new Note("F", 9),
+			new Note("F#", 10),	new Note("G", 11),	new Note("G#", 12)
 		];
 	}
 
 	static makeNotesFlat() {
 		return [
-			new Note("Ab", 1, true), new Note("A", 2, true), new Note("Bb", 3, false),
-			new Note("B", 4, true), new Note("C", 5, true), new Note("Db", 6, true),
-			new Note("D", 7, true), new Note("Eb", 8, false), new Note("E", 9, true),
-			new Note("F", 10, true), new Note("Gb", 11, true), new Note("G", 12, true)
+			new Note("Ab", 1),	new Note("A", 2),	new Note("Bb", 3),
+			new Note("B", 4),	new Note("C", 5),	new Note("Db", 6),
+			new Note("D", 7),	new Note("Eb", 8),	new Note("E", 9),
+			new Note("F", 10),	new Note("Gb", 11),	new Note("G", 12)
 		];
 	}
 }
 
+// # PUBLIC const
+const accentedFret = [0, 3, 5, 7, 9, 12, 15, 17, 19, 21, 24];
+const svgContainer = document.getElementById("svgFretboard");
+const tuningInput = document.getElementById("tuning");
+const fretsNrInput = document.getElementById("fretsNr");
+const frets = parseInt(fretsNrInput.value);
+const svgNs = "http://www.w3.org/2000/svg";
+const circleClassName = "svgDots";
+const svgLineStringClassName = "svgLineString";
+const svgRectZeroFretSpacerWidth = 20; /* Change in CSS: svgRectZeroFretSpacer. */
+const svgRectWidth = 50; /* Change in CSS: svgRect. */
+const pushXby = svgRectWidth + 10;
+const pushYAfterFretNumbersBy = 40;
+const dotR = 6;
+
+// # PUBLIC var
+var rectX = svgRectWidth; /* Default, fallback. */
+var rectY = 20; /* Default, fallback. */
+var textX = svgRectWidth + (svgRectWidth / 2); /* Default, fallback. */
+var textY = 48; /* Default, fallback. */
+
+function svgResetXandY(){
+	rectX = svgRectWidth;
+	rectY = 20;
+	textX = svgRectWidth + (svgRectWidth / 2);
+	textY = 48;
+}
+
+// # Handlers and events:
+document.getElementById("applyTuning").addEventListener("click", updateFretboard);
+document.getElementById("lefthanded").addEventListener("change", updateFretboard);
+document.getElementById("useFlats").addEventListener("change", updateFretboard);
+document.getElementById("omitSharpsFlats").addEventListener("change", updateFretboard);
+
 function generateFretboard(frets, strings, lefthanded, useFlats, omitAccidentals) {
 	
-	// ## CONSTS:
+	// ## Local CONSTS:
 	const notes = useFlats ? Note.makeNotesFlat() : Note.makeNotesSharp();
-	const accentedFret = [0, 3, 5, 7, 9, 12, 15, 17, 19, 21, 24];
-	const svgContainer = document.getElementById("svgFretboard");
 	const labelRange = Array.from({ length: frets + 1 }, (_, i) => i);
 	
+	// ## Clear+reset container and svg elements:
 	svgClear(svgContainer);
 	svgResetXandY();
 	
@@ -38,41 +71,40 @@ function generateFretboard(frets, strings, lefthanded, useFlats, omitAccidentals
 	if(frets > 24) frets = 24;
 	if(frets < 1) frets = 1;
 
-	// ## Fret labels: normal order or reversed if left-handed:
+	// ## Generate fret numbers (top):
 	if (lefthanded) labelRange.reverse();
 	labelRange.forEach(i => {
 		
 		if((!lefthanded && i == 1) || (lefthanded && i == 0))
 		{
-			var elGZ = document.createElementNS("http://www.w3.org/2000/svg", "g");
+			var elGZ = document.createElementNS(svgNs, "g");
 			var elRectZ = svgCreateRect(rectX, rectY, "svgRectZeroFretSpacer");
 			elGZ.appendChild(elRectZ);
 			var elTextZ = svgCreateText(textX, textY, textClassName, "");
 			elGZ.appendChild(elTextZ);
 			svgContainer.appendChild(elGZ);
-			rectX += 20;
-			textX += 20;
+			rectX += svgRectZeroFretSpacerWidth;
+			textX += svgRectZeroFretSpacerWidth;
 		}
 		
-		var elG = document.createElementNS("http://www.w3.org/2000/svg", "g");
-		var rectClassName = "svgRectNumbers";
+		var elG = document.createElementNS(svgNs, "g");
 		var textClassName = "svgTextNumbers";
 		var textValue = "";
 		
-		var elRect = svgCreateRect(rectX, rectY, rectClassName);
-		elG.appendChild(elRect);
 		var elText = svgCreateText(textX, textY, textClassName, i);
 		elG.appendChild(elText);
 		svgContainer.appendChild(elG);
-		rectX += 60;
-		textX += 60;
+		rectX += pushXby;
+		textX += pushXby;
 	});
-	rectX = 50;
-	textX = 75;
-	rectY += 40;
-	textY += 40;
+	
+	// ## Reposition x and y for a new row:
+	rectX = svgRectWidth;
+	textX = svgRectWidth + (svgRectWidth / 2);
+	rectY += pushYAfterFretNumbersBy;
+	textY += pushYAfterFretNumbersBy;
 
-	// ## Use strings as-is (no reversing):
+	// ## Generate notes:
 	strings.forEach(baseNote => {
 		const startIndex = notes.findIndex(n => n.Note === baseNote);
 		let finalNotes = notes.slice(startIndex);
@@ -81,31 +113,31 @@ function generateFretboard(frets, strings, lefthanded, useFlats, omitAccidentals
 			finalNotes = finalNotes.concat(notes);
 		}
 
-		// ## Reverse notes only if left-handed:
+		// ### Reverse notes only if left-handed:
 		if (lefthanded) finalNotes.reverse();
-		const sliced = lefthanded ? finalNotes.slice(-frets - 1) : finalNotes.slice(0, frets + 1);
+		const slicedNotes = lefthanded ? finalNotes.slice(-frets - 1) : finalNotes.slice(0, frets + 1);
 
-		nt = lefthanded ? frets : 0;
-		sliced.forEach(note => {
-			const isAccidental = note.Note.includes("#") || note.Note.includes("b");
+		noteIdx = lefthanded ? frets : 0;
+		slicedNotes.forEach(note => {
+			var isAccidental = note.Note.includes("#") || note.Note.includes("b");
 			
-			if((!lefthanded && nt == 1) || (lefthanded && nt == 0))
-			{
-				var elGZ = document.createElementNS("http://www.w3.org/2000/svg", "g");
+			// #### Make some space after zero-fret:
+			if((!lefthanded && noteIdx == 1) || (lefthanded && noteIdx == 0)) {
+				var elGZ = document.createElementNS(svgNs, "g");
 				var elRectZ = svgCreateRect(rectX, rectY, "svgRectZeroFretSpacer");
 				elGZ.appendChild(elRectZ);
 				var elTextZ = svgCreateText(textX, textY, textClassName, "");
 				elGZ.appendChild(elTextZ);
 				svgContainer.appendChild(elGZ);
-				rectX += 20;
-				textX += 20;
+				rectX += svgRectZeroFretSpacerWidth;
+				textX += svgRectZeroFretSpacerWidth;
 			}
 			
 			var rectClassName = "svgRect";
 			var textClassName = "svgText";
 			var textValue = "";
 
-			// Omit accidentals, and Each Note should have a different color:
+			// #### Omit accidentals, and Each Note should have a different color:
 			if (isAccidental && omitAccidentals)
 			{
 				textValue = " ";
@@ -122,7 +154,7 @@ function generateFretboard(frets, strings, lefthanded, useFlats, omitAccidentals
 				rectClassName += " note-" + note.Note.replace("b", "").replace("#", "").toLowerCase();
 			}
 			
-			var elG = document.createElementNS("http://www.w3.org/2000/svg", "g");
+			var elG = document.createElementNS(svgNs, "g");
 			
 			var elRect = svgCreateRect(rectX, rectY, rectClassName);
 			elG.appendChild(elRect);
@@ -130,55 +162,54 @@ function generateFretboard(frets, strings, lefthanded, useFlats, omitAccidentals
 			var elText = svgCreateText(textX, textY, textClassName, textValue);
 			elG.appendChild(elText);
 			
-			var elLine = svgCreateLineString(rectX+50, rectY+(50/2), rectX+60, rectY+(50/2), "svgLineString");
+			var elLine = svgCreateLineString(rectX+svgRectWidth, rectY+(svgRectWidth/2), rectX+pushXby, rectY+(svgRectWidth/2), svgLineStringClassName);
 			elG.appendChild(elLine);
 			
 			svgContainer.appendChild(elG);
-			rectX += 60;
-			textX += 60;
 			
-			lefthanded ? nt-- : nt++;
+			rectX += pushXby;
+			textX += pushXby;
+			
+			// #### (In/De)crement note index:
+			lefthanded ? noteIdx-- : noteIdx++;
 		});
-		rectX = 50;
-		textX = 75;
-		rectY += 60;
-		textY += 60;
+		rectX = svgRectWidth;
+		textX = svgRectWidth + (svgRectWidth / 2);
+		rectY += pushXby;
+		textY += pushXby;
 	});
 	
-	// DOTS:
+	// ## Generate dots (3, 5, 7...):
 	var circleX = 75;
 	labelRange.forEach(i => {
 		if (accentedFret.includes(i)) {
 			var dbl = i == 12 || i == 24;
 			if (dbl) {
-				var elG = document.createElementNS("http://www.w3.org/2000/svg", "g");
-
-				var circleClassName = "svgDots";
+				var elG = document.createElementNS(svgNs, "g");
 				
-				var elCircle = svgCreateCircle(6, circleX-12, textY-25, circleClassName, dbl);
+				var elCircle = svgCreateCircle(dotR, circleX-(dotR*2), textY-(svgRectWidth / 2), circleClassName, dbl);
 				elG.appendChild(elCircle);
-				svgContainer.appendChild(elG);	
+				svgContainer.appendChild(elG);
 			
-				var elCircle = svgCreateCircle(6, circleX+12, textY-25, circleClassName, dbl);
+				elCircle = svgCreateCircle(dotR, circleX+(dotR*2), textY-(svgRectWidth / 2), circleClassName, dbl);
 				elG.appendChild(elCircle);
 				svgContainer.appendChild(elG);	
 			}
 			else {
-				var elG = document.createElementNS("http://www.w3.org/2000/svg", "g");
-				var circleClassName = "svgDots";
+				var elG = document.createElementNS(svgNs, "g");
 				
-				var elCircle = svgCreateCircle(6, circleX, textY-25, circleClassName, dbl);
+				var elCircle = svgCreateCircle(dotR, circleX, textY-(svgRectWidth / 2), circleClassName, dbl);
 				elG.appendChild(elCircle);
 				svgContainer.appendChild(elG);			
 			}
 		}
-		rectX += (i == 0 ? 80 : 60);
-		circleX += (i == 0 ? 80 : 60);
+		rectX += (((i == 1 && lefthanded) || (i == 2 && !lefthanded)) ? pushXby+svgRectZeroFretSpacerWidth : pushXby);
+		circleX += (((i == 1 && lefthanded) || (i == 2 && !lefthanded)) ? pushXby+svgRectZeroFretSpacerWidth : pushXby);
 	});
 }
 
 function svgCreateRect(x, y, className, dbl){
-	var el = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+	var el = document.createElementNS(svgNs, "rect");
 	el.setAttribute("x", x);
 	el.setAttribute("y", y);
 	el.setAttribute("rx", "5");
@@ -188,7 +219,7 @@ function svgCreateRect(x, y, className, dbl){
 }
 
 function svgCreateText(x, y, className, txt){
-	var el = document.createElementNS("http://www.w3.org/2000/svg", "text");
+	var el = document.createElementNS(svgNs, "text");
 	el.setAttribute("x", x);
 	el.setAttribute("y", y);
 	el.setAttribute("class", className);
@@ -198,7 +229,7 @@ function svgCreateText(x, y, className, txt){
 }
 
 function svgCreateCircle(r, cx, cy, className){
-	var el = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	var el = document.createElementNS(svgNs, "circle");
 	el.setAttribute("r", r);
 	el.setAttribute("cx", cx);
 	el.setAttribute("cy", cy);
@@ -213,7 +244,7 @@ function svgClear(svgContainer){
 }
 
 function svgCreateLineString(x1, y1, x2, y2, className) {
-	var el = document.createElementNS("http://www.w3.org/2000/svg", "line");
+	var el = document.createElementNS(svgNs, "line");
 	el.setAttribute("x1", x1);
 	el.setAttribute("y1", y1);
 	el.setAttribute("x2", x2);
@@ -221,36 +252,16 @@ function svgCreateLineString(x1, y1, x2, y2, className) {
 	el.setAttribute("class", className);
 	return el;
 }
-
-function svgResetXandY(){
-	rectX = 50;
-	rectY = 20;
-	textX = 75;
-	textY = 48;
-}
-
-const tuningInput = document.getElementById("tuning");
-const fretsNrInput = document.getElementById("fretsNr");
-var rectX = 50; /* Default, fallback. */
-var rectY = 20; /* Default, fallback. */
-var textX = 75; /* Default, fallback. */
-var textY = 48; /* Default, fallback. */
 	
-// ## Entry:
+// ## Main function:
 function updateFretboard() {
 	const lefthanded = document.getElementById("lefthanded").checked;
 	const useFlats = document.getElementById("useFlats").checked;
 	const omitAccidentals = document.getElementById("omitSharpsFlats").checked;
 	const tuning = tuningInput.value.trim().split(/\s+/).reverse();
-	const frets = parseInt(fretsNrInput.value);
+	
 	generateFretboard(frets, tuning, lefthanded, useFlats, omitAccidentals);
 }
-
-// ## Handlers and events:
-document.getElementById("applyTuning").addEventListener("click", updateFretboard);
-document.getElementById("lefthanded").addEventListener("change", updateFretboard);
-document.getElementById("useFlats").addEventListener("change", updateFretboard);
-document.getElementById("omitSharpsFlats").addEventListener("change", updateFretboard);
 
 // ## Main:
 updateFretboard();
